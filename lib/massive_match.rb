56f7e58837acb7560dd73c979ade6c34ad317707
Jake Sower <jakesower@gmail.com>
2014-03-19 15:00:19 -0400

    including flat selection classes

--- lib/massive_match.rb
+++ lib/massive_match.rb
@@ -6,4 +6,6 @@ module MassiveMatch
   require 'massive_match/constraint.rb'
   require 'massive_match/tuple_match.rb'
   require 'massive_match/variable_set.rb'
+  require 'massive_match/selection_match.rb'
+  require 'massive_match/flat_variable_set.rb'
 end
